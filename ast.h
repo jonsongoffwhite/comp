5c7ac7b760f95e65141d358f3523da562e5e1f8a
Jonson <jonnygoffwhite@gmail.com>
2025-09-06 21:33:03 +0100

    Add statements and expressions

--- ast.h
+++ ast.h
@@ -1,8 +1,9 @@
 #ifndef AST_H
 #define AST_H
 
-typedef struct Stmt stmt;
-typedef struct Expr expr;
+#include "lexer.h"
+typedef struct stmt Stmt;
+typedef struct expr Expr;
 
 typedef enum { STMT_VAR_DECL, STMT_FUN_DECL, STMT_EXPR } StmtType;
 
@@ -12,5 +13,64 @@ typedef enum {
   EXPR_BINARY,
   EXPR_ID
 } ExprType;
+
+typedef struct {
+  char *type; // TODO: Enum?
+  char *id;
+  Expr *value;
+} VarDeclStmt;
+
+// Better way to model list of params?
+// TODO: Allow >1 params
+// TODO: Allow multiple statements.
+typedef struct {
+  char *id;
+  char *param_type;
+  char *param_id;
+  Expr *value;
+} FunDeclStmt;
+
+typedef struct {
+  Expr *value;
+} ExprStmt;
+
+struct stmt {
+  StmtType type;
+  union {
+    VarDeclStmt var_decl;
+    FunDeclStmt fun_decl;
+    ExprStmt expr;
+  };
+};
+
+//
+
+typedef struct {
+  char *val;
+} IntLiteralExpr;
+
+typedef struct {
+  char *val;
+} StringLiteralExpr;
+
+typedef struct {
+  Token op;
+  Expr *left;
+  Expr *right;
+} BinaryExpr;
+
+typedef struct {
+  char *id;
+} IdExpr;
+
+struct expr {
+  ExprType type;
+  union {
+    IntLiteralExpr int_lit;
+    StringLiteralExpr str_lit;
+    BinaryExpr binary;
+    IdExpr id;
+  };
+};
 
 #endif // !AST_H
--- parser.c
+++ parser.c
@@ -1,6 +1,9 @@
 #include "parser.h"
+#include "ast.h"
 #include "lexer.h"
 #include <stdio.h>
+
+Expr *parse_exp(TokenData *start);
 
 int parse(Parser *parser) {
   while (parser->curr_tok->type != TOK_EOF) {
@@ -9,6 +12,19 @@ int parse(Parser *parser) {
     case TOK_TYPE:
       // parse variable declaration
       printf("variable declaration\n");
+      char *type = parser->curr_tok->val;
+      char *id = (++parser->curr_tok)->val;
+
+      if ((++parser->curr_tok)->type != TOK_EQ) {
+        printf("no equals '=' in variable declaration");
+        return 1;
+      }
+
+      // Rest goes to exp parser
+      parse_exp(++parser->curr_tok);
+
+      // TODO: evaluate rest as expression
+
       while (parser->curr_tok->type != TOK_SEMICOL) {
         parser->curr_tok++;
       }
@@ -35,4 +51,12 @@ int parse(Parser *parser) {
     }
   }
   return 0;
+}
+
+Expr *parse_exp(TokenData *start) {
+  while (start->type != TOK_SEMICOL) {
+    printf("parse exp tok: %s: %s\n", token_to_string(start->type), start->val);
+    start++;
+  }
+  return NULL;
 }
